c9aa391f2ff4d23820e43c4c00d5c6542fb32e5d
Aaron Yu <aaron.yuu@outlook.com>
2021-09-16 18:26:39 +0800

    mv java file into a folder.

--- cobra-unless/cmd/version.go
+++ cobra-unless/cmd/version.go
@@ -5,7 +5,16 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
+
+type FlagError struct {
+	Err error
+}
+
+func (f *FlagError) Error() string {
+	return "nil"
+}
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
@@ -18,12 +27,23 @@ var versionCmd = &cobra.Command{
 
 func NewCmdVersion(version, buildDate string) *cobra.Command {
 	cmd := &cobra.Command{
+		DisableFlagsInUseLine: true,
+
 		Use:    "version",
 		Hidden: true,
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Fprintf(os.Stdout, version, buildDate)
 		},
 	}
+
 	// cmdutil.DisableAuthCheck(cmd)
+
+	cmd.SetFlagErrorFunc(func(cmd *cobra.Command, err error) error {
+		if err == pflag.ErrHelp {
+			return err
+		}
+		return &FlagError{Err: fmt.Errorf("%w\nSeparate flags with '--'.", err)}
+	})
+
 	return cmd
 }
