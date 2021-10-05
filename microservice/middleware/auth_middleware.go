dbea6ceeeb0c80439d5f581809e5de610799b432
Aaron Yu <aaron.yuu@outlook.com>
2021-10-05 18:48:14 +0800

    rename middleware files.

--- microservice/middleware/auth_middleware.go
+++ microservice/middleware/auth_middleware.go
@@ -5,33 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"runtime/debug"
-	"time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Success!!"))
-}
-
-func Logging(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		next.ServeHTTP(w, r)
-		log.Printf("%s %s %s", r.Method, r.RequestURI, time.Since(start))
-	})
-}
-
-func PanicRecovery(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if err := recover(); err != nil {
-				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-				log.Println(string(debug.Stack()))
-			}
-		}()
-		next.ServeHTTP(w, r)
-	})
 }
 
 const UserContextKey = "user"
--- microservice/middleware/logging_middleware.go
+++ microservice/middleware/logging_middleware.go
@@ -34,6 +34,14 @@ func anotherLoggingMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+func Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf("%s %s %s", r.Method, r.RequestURI, time.Since(start))
+	})
+}
+
 func main() {
 
 	ps := &PoliteServer{}
