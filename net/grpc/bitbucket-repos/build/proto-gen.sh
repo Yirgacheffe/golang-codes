12c4b8c0a4a01274be7432ba35e278649f4b861c
Aaron Yu <aaron.yuu@outlook.com>
2021-10-22 00:26:57 +0800

    comment proto-gen file.

--- net/grpc/bitbucket-repos/build/proto-gen.sh
+++ net/grpc/bitbucket-repos/build/proto-gen.sh
@@ -1,2 +1,5 @@
+## 
+## proto-gen.sh should be run outside of 'bitbucket-repos' folder
+## 
 protoc -I . --go_out=. ./bitbucket-repos/internal/proto-files/domain/repository.proto
 protoc -I . --go_out=plugins=grpc:. ./bitbucket-repos/internal/proto-files/service/repository-service.proto
