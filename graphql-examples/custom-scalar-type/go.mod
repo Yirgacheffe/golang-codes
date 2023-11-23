28fe7743cc9480e8eafa9378841a48a3c7327ebe
Aaron Yu <aaron.yuu@outlook.com>
2023-11-23 11:15:07 +0800

    update customer type.

---
+++ graphql-examples/custom-scalar-type/go.mod
@@ -0,0 +1,3 @@
+module custom-scalar-type
+
+go 1.21.3
--- graphql-examples/custom-scalar-type/main.go
+++ graphql-examples/custom-scalar-type/main.go
@@ -14,6 +14,53 @@ func NewCustomID(v string) *CustomID {
 	return &CustomID{value: v}
 }
 
+var CustomScalarType = graphql.NewScalar(graphql.ScalarConfig{
+	Name:        "CustomScalarType",
+	Description: "The `CustomScalarType` scalar type represents an ID Object",
+	Serialize: func(value interface{}) interface{} {
+		switch value := value.(type) {
+		case CustomID:
+			return value.String()
+		case *CustomID:
+			v := *value
+			return v.String()
+		default:
+			return nil
+		}
+	},
+	ParseValue: func(value interface{}) interface{} {
+		switch value := value.(type) {
+		case string:
+			return NewCustomID(value)
+		case *string:
+			return NewCustomID(*value)
+		default:
+			return nil
+		}
+	},
+	ParseLiteral: func(valueAST ast.Value) interface{} {
+		switch valueAST := valueAST.(type) {
+		case *ast.StringValue:
+			return NewCustomID(valueAST.Value)
+		default:
+			return nil
+		}
+	},
+})
+
+type Customer struct {
+	ID *CustomID `json:"id"`
+}
+
+var CustomerType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Customer",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: CustomerScalarType,
+		},
+	},
+})
+
 func main() {
 	fmt.Println("hello")
 }
