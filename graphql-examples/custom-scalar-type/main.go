bb4bb436b1f22d7bb5a6e16bc1e948f38e31506b
Aaron Yu <aaron.yuu@outlook.com>
2023-11-23 11:30:25 +0800

    update main for customer type query.

--- graphql-examples/custom-scalar-type/main.go
+++ graphql-examples/custom-scalar-type/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
 type CustomID struct {
 	value string
@@ -62,5 +66,56 @@ var CustomerType = graphql.NewObject(graphql.ObjectConfig{
 })
 
 func main() {
-	fmt.Println("hello")
+	schema, err := graghql.NewSchema(graphql.SchemaConfig{
+		Query: graphql.NewObject(graphql.ObjectConfig{
+			Name: "Query",
+			Fields: graphql.Fields{
+				"customers": &graphql.Field{
+					Type: graphql.NewList(CustomerType),
+					Args: graphql.FieldConfigArgument{
+						"id": &graphql.ArgumentConfig{Type: CustomScalarType},
+					},
+					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						// id := p.Args["id"]
+						log.Printf("id from arguments: %+v", id)
+						customers := []Customer{
+							Customer{ID: NewCustomID("fb8378783f")},
+						}
+						return customers, nil
+					},
+				},
+			},
+		}),
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := `
+		query{
+			customers {
+				id
+			}
+		}
+	`
+
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+		VariableValues: map[string]interface{}{
+			"id": "7367748fsb3",
+		},
+	})
+
+	if len(result.Errors) > 0 {
+		log.Fatal(result)
+	}
+
+	b, err := json.Marshal(result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(b))
 }
